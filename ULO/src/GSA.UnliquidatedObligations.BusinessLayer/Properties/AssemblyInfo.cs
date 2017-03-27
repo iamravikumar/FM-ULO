66aee6d03b5d750796f6ed9bf9b4444a270e393f
CharlesGWatson <gary.watson@gsa.gov>
2017-03-27 17:09:45 -0400

    Adding Web and business layer project redo part 2

--- ULO/src/GSA.UnliquidatedObligations.BusinessLayer/Properties/AssemblyInfo.cs
+++ ULO/src/GSA.UnliquidatedObligations.BusinessLayer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f896cffa-862c-4f1a-9c02-b751478d7eeb")]
+[assembly: Guid("dec1ac2c-ed6f-494c-996d-9f58e0ad5112")]
 
 // Version information for an assembly consists of the following four values:
 //
