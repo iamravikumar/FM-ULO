18785543bfb4db34c64159a7c7d5a76cdfa89a9e
Jason <jason@jasonthomas.com>
2017-03-28 10:22:56 -0400

    Scripts to create the underlying database tables.  We're using a database first design pattern.

---
+++ ULO/src/GSA.UnliquidatedObligations.BusinessLayer/DatabaseScripts/0006.StringSplit.cs.sql
@@ -0,0 +1,30 @@
+﻿CREATE FUNCTION SplitString(@csv_str NVARCHAR(4000), @delimiter nvarchar(20))
+ RETURNS @splittable table (val nvarchar(max), pos int)
+AS
+BEGIN  
+ 
+-- Check for NULL string or empty sting
+    IF  (LEN(@csv_str) < 1 OR @csv_str IS NULL)
+    BEGIN
+        RETURN
+    END
+ 
+    ; WITH csvtbl(i,j, pos)
+    AS
+    (
+        SELECT i=1, j= CHARINDEX(@delimiter,@csv_str+@delimiter), 1
+ 
+        UNION ALL 
+ 
+        SELECT i=j+1, j=CHARINDEX(@delimiter,@csv_str+@delimiter,j+1), pos+1
+        FROM csvtbl
+        WHERE CHARINDEX(@delimiter,@csv_str+@delimiter,j+1) <> 0
+    )   
+    INSERT  INTO @splittable(val, pos)
+    SELECT  SUBSTRING(@csv_str,i,j-i), pos
+    FROM    csvtbl 
+ 
+    RETURN
+END  
+
+GO
---
+++ ULO/src/GSA.UnliquidatedObligations.BusinessLayer/DatabaseScripts/0200.gsa.sql
@@ -0,0 +1,48 @@
+﻿create schema gsa
+
+GO
+
+create table gsa.Zones
+(
+	ZoneId int not null primary key,
+	ZoneName varchar(100) not null unique
+)
+
+GO
+
+insert into gsa.Zones
+(ZoneId, ZoneName)
+values
+(1, 'Zone 1'),
+(2, 'Zone 2'),
+(3, 'Zone 3'),
+(4, 'Zone 4');
+
+GO
+
+create table gsa.Regions
+(
+	RegionId int not null primary key,
+	ZoneId int not null references gsa.Zones(ZoneId),
+	RegionNumber varchar(10) not null unique,
+	RegionName varchar(100) not null unique
+)
+
+GO
+
+insert into gsa.Regions
+(RegionId, ZoneId, RegionNumber, RegionName)
+values
+(1 , 1, '1' , 'New England'),
+(2 , 1, '2' , 'Northeast & Caribbean'),
+(3 , 1, '3' , 'Mid-Atlantic'),
+(4 , 2, '4' , 'Southeast Sunbelt'),
+(5 , 1, '5' , 'Great Lakes'),
+(6 , 2, '6' , 'Heartland'),
+(7 , 2, '7' , 'Greater Southwest'),
+(8 , 3, '8' , 'Rocky Mountain'),
+(9 , 3, '9' , 'Pacific Rim'),
+(10, 3, '10', 'Northwest / Arctic'),
+(11, 4, '11', 'National Capital Region');
+GO
+
