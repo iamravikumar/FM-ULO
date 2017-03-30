39e96bc69e100393df6d82bad18e94ae57aafee4
CharlesGWatson <gary.watson@gsa.gov>
2017-03-30 13:35:15 -0400

    Adding unit tests projects

---
+++ ULO/tests/GSA.UnliquidatedObligations.BusinessLayer.Tests/Workflow/ClaimHelpersTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using GSA.UnliquidatedObligations.BusinessLayer.Authorization;
+using GSA.UnliquidatedObligations.BusinessLayer.Data;
+using GSA.UnliquidatedObligations.BusinessLayer.Workflow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GSA.UnliquidatedObligations.BusinessLayer.Tests.Workflow
+{
+    [TestClass]
+    public class ClaimHelperTests
+    {
+
+        [TestMethod]
+        public void GetClaimsTest()
+        {
+            var guid = Guid.NewGuid().ToString();
+            var testUser = new AspNetUser
+            {
+                Id = guid,
+                AspNetUserClaims = new List<AspNetUserClaim>
+                {
+                    new AspNetUserClaim()
+                    {
+                        Id = 1,
+                        UserId = guid,
+                        ClaimType = "testClaimType",
+                        ClaimValue = "testClaimValue"
+                    },
+                    new AspNetUserClaim()
+                    {
+                        Id = 2,
+                        UserId = guid,
+                        ClaimType = "testClaimType2",
+                        ClaimValue = "testClaimValue2"
+                    },
+                    new AspNetUserClaim()
+                    {
+                        Id = 3,
+                        UserId = guid,
+                        ClaimType = "testClaimType3",
+                        ClaimValue = "testClaimValue3"
+                    }
+                }
+            };
+
+            var claims = testUser.GetClaims();
+            Assert.AreEqual(3, claims.Count);
+
+
+        }
+    }
+}
---
+++ ULO/tests/GSA.UnliquidatedObligations.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using GSA.UnliquidatedObligations.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GSA.UnliquidatedObligations.Web.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void IndexTest()
+        {
+            HomeController controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("", result.ViewName);
+        }
+    }
+}
