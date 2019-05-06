0eccb4b137df3096ff6766b97a7bb4182e322eda
Jason Thomas <jason@jasonthomas.com>
2019-05-06 13:23:56 -0400

    Intermediate web apps to test publishing to linux.  These will be squashed soon.

---
+++ ULO/WebCoreL2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebCoreL2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
